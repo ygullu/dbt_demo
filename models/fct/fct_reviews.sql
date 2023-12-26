dadd4e550ca1553111dda92cf8824d983e141ca1
annem.sujatha@gmail.com <annem.sujatha@gmail.com>
2023-12-26 14:28:59 +0000

    commiting test changes.

--- models/fct/fct_reviews.sql
+++ models/fct/fct_reviews.sql
@@ -1,14 +1,14 @@
-{{
-config(
-materialized = 'incremental',
-on_schema_change='fail'
-)
-}}
-WITH src_reviews AS (
-SELECT * FROM {{ ref('src_reviews') }}
-)
-SELECT * FROM src_reviews
-WHERE review_text is not null
-{% if is_incremental() %}
-AND review_date > (select max(review_date) from {{ this }})
-{% endif %}
+{{ config(materialized="incremental", on_schema_change="fail") }}
+with src_reviews as (select * from {{ ref("src_reviews") }})
+select
+    {{
+        dbt_utils.generate_surrogate_key(
+            ["listing_id", "review_date", "reviewer_name", "review_text"]
+        )
+    }} as review_id, *
+from src_reviews
+where
+    review_text is not null
+    {% if is_incremental() %}
+        and review_date > (select max(review_date) from {{ this }})
+    {% endif %}
